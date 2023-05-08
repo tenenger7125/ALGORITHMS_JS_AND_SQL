9cecbf9ee4060c2b5251705f515e47d89c1cc5ac
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-08 17:40:24 +0900

    [unrated] Title: 배열 만들기 4, Time: 9.52 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    let stk = [];
+    let i = 0;
+    
+    while (i < arr.length) {
+        if (stk.length === 0) stk.push(arr[i++]);
+        if (stk.length !== 0 && (stk.at(-1) < arr[i])) stk.push(arr[i++]);
+        if (stk.length !== 0 && (stk.at(-1) >= arr[i])) stk.pop();
+    }
+    
+    return stk;
+}
