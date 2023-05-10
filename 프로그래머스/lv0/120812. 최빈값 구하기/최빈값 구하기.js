348eafdfa3968d30b7c503976bec535b28045742
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-11 03:29:44 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -4,7 +4,5 @@ function solution(array) {
         return acc
     }, {})).sort((a,b) => b[1]-a[1]);
     
-    if (entry.length === 1) return +entry[0][0]
-    
-    return entry[0][1] === entry[1][1] ? -1 : +entry[0][0];
+    return entry.length === 1 || entry[0][1] > entry[1][1] ? +entry[0][0] : -1;
 }
