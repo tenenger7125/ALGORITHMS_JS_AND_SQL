fd0441a9e29c097be1ff7f430b9e3e44e39f47cc
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-10 20:38:42 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,8 +1,5 @@
 function solution(num_list) {
-    const {odd, even} = num_list.reduce((acc, cur, idx) => {
-        Math.abs(idx % 2) === 0 ? acc.even += cur : acc.odd += cur
-        return acc;
-    }, {odd: 0, even: 0});
-    
-    return odd > even ? odd : even;
+    return Math.max(...num_list.reduce(([odd, even], cur, idx) => {
+        return idx % 2 === 0 ? [odd, even + cur]  : [odd + cur, even]
+    }, [0, 0]));
 }
