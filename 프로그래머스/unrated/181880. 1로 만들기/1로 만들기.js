01fd766924edadae9f0a85500c983ee9588d6d3c
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-11 03:17:35 +0900

    [unrated] Title: 1로 만들기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -1,11 +1,3 @@
 function solution(num_list) {
-    return num_list.reduce((acc, cur) => {
-        let count = 0;
-        
-        while (cur !== 1) {
-            cur = cur % 2 === 0 ? cur / 2 : (cur - 1) / 2
-            count++
-        }
-        return acc + count
-    }, 0)
+    return num_list.reduce((acc, cur) => acc + cur.toString(2).length - 1, 0)
 }
