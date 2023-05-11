a784b3615687f035acca0c37d5db7b56fd94f6f3
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-12 02:47:00 +0900

    [unrated] Title: 세 개의 구분자, Time: 9.77 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,5 +1,3 @@
 function solution(myStr) {
-    const arr = myStr.replace(/[abc]/g, '*').split('*').filter(str => str);
-    
-    return arr.length === 0 ? ['EMPTY'] : arr;
+    return myStr.match(/[^a-c]+/g) || ['EMPTY']
 }
