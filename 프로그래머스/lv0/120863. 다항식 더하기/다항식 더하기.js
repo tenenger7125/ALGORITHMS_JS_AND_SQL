0ff42d06fa09ce9d12a49f30675bbac20f771eef
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-12 01:51:23 +0900

    [level 0] Title: 다항식 더하기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,19 +1,13 @@
 function solution(polynomial) {
-    const arr = polynomial.split(" ").filter(el => el !== '+');
-
-    const obj = arr.reduce((acc, cur) => {       
-        const num = cur.replace('x', '') === '' ? 1 : parseInt(cur)
-        
-        cur.includes('x') ? acc.x += num : acc.n += +cur;
+    const obj = polynomial.split(" + ").reduce((acc, cur) => {       
+        cur.includes('x') ? acc.x += parseInt(cur.replace('x', '') || 1) : acc.n += +cur;
         
         return acc
     }, {x: 0, n: 0})
     
-    return Object.entries(obj).sort((a,b) => a-b).reduce((acc, cur) => {
-        const format = cur.reverse().join('').replace('n', '')
+    return Object.entries(obj).reduce((acc, [key, value]) => {
+        if (value === 0 || (key === 'x' && value === 0)) return acc
         
-        if (format === '0' || format.includes('x') && format[0] === '0') return acc
-        
-        return (format.includes('x') && parseInt(format) === 1) ? [...acc, 'x'] : [...acc, format]
+        return (key === 'x' && value === 1) ? [...acc, key] : [...acc, key === 'x' ? value + key : value]
     }, []).join(' + ');
 }
