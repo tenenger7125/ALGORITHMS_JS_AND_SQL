b483a74b8337233e0f9b64072315491647c06372
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-12 03:22:24 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,5 +1,3 @@
 function solution(date1, date2) {
-    const [[year1, month1, day1], [year2, month2, day2]] = [date1, date2];
-    
-    return (year1 < year2 || (year1 === year2 && month1 < month2) || (year1 === year2 && month1 === month2 && day1 < day2)) ? 1 : 0
+    return +(new Date(date1) < new Date(date2))
 }
