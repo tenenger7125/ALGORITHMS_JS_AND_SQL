a6ba334b56ec26314fb34d253ab5ac9f00efe4d1
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-13 15:32:27 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -0,0 +1,5 @@
+function solution(dots) {
+    const [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots;
+    
+    return (Math.max(x1,x2,x3,x4) - Math.min(x1,x2,x3,x4)) * (Math.max(y1,y2,y3,y4) - Math.min(y1,y2,y3,y4));
+}
