5064fa4948c3d605e1189ba613e17cc6954266c8
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-13 14:27:05 +0900

    [level 0] Title: 소인수분해, Time: 1.42 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,19 @@
+function getPrimaryArray (n) {
+    const set = new Set(Array.from({length: n}, (_, i) => i+1));
+    
+    for (let i=1; i<=Math.sqrt(n); i++) {
+        if (i === 1) set.delete(i);
+        
+        if (set.has(i)) {
+            for (let j=i*2; j<=n; j += i) {
+                set.delete(j)
+            }
+        }
+    }
+
+    return [...set];
+}
+
+function solution(n) {
+    return getPrimaryArray(n).filter(primary => n % primary === 0);
+}
