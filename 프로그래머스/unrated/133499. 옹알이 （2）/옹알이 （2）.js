7804d2a592734ec676cc03151c3e0aabb666b82d
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-15 00:06:00 +0900

    [unrated] Title: 옹알이 (2), Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,11 +1,10 @@
-const possibleBabbling = ["aya", "ye", "woo", "ma"];
-
 function solution(babbling) {
-    return babbling.filter(babble => {
-        return possibleBabbling.reduce((acc, cur) => {
-            const result = acc.replace(new RegExp(cur, 'g'), '@')
-            
-            return /@{2,}/.test(result) ? acc : result.replace(/@/g, '*')
-        }, babble).replace(/\*/g, '') === ''
-    }).length
+    // 다른 사람의 풀이 입니다.
+    const regex1 = /(aya|ye|woo|ma)\1+/; // aya, ye, woo, ma가 연속으로 되는지 확인 용도
+    const regex2 = /^(aya|ye|woo|ma)+$/; // aya, ye, woo, ma로만 문자열이 구성되어 있는지 확인 용도
+
+    return babbling.reduce((ans, word) => {
+        // 문자열이 연속되어 있지 않거나, 특정 문자열로만 되어있는 경우에만 숫자를 +1한다.
+        return !regex1.test(word) && regex2.test(word) ? ++ans : ans
+    }, 0);
 }
