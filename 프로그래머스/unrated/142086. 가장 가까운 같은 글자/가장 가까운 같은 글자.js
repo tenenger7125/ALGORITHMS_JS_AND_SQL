813f8ea10ccfd3c37b8a52d48335b1df7ddd13e1
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-18 01:43:32 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.16 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,7 +1,7 @@
 function solution(s) {
     return [...s].map((char, idx) => {
         const targetIdx = s.slice(0, idx).lastIndexOf(char);
-        
-        return targetIdx === -1 ? -1 : s.slice(0, idx).length - s.slice(0, idx).lastIndexOf(char);
+
+        return targetIdx === -1 ? -1 : idx - targetIdx;
     })
 }
