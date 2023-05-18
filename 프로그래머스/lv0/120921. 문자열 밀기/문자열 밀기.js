c708a70b8b37e93641547caebda17c71eeff185f
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-18 12:41:34 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,11 +1,3 @@
 function solution(A, B) {
-    let result = A;
-    
-    for (let i=0; i<A.length; i++) {
-        if (result === B) return i;
-        
-        result = result.at(-1) + result.slice(0, -1)
-    }
-    
-    return -1;
+    return (B+B).indexOf(A)
 }
