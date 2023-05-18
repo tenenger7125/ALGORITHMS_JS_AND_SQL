ee2d113f185532fee94aed0f1c264c4c02eac785
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-18 12:32:09 +0900

    [level 0] Title: 특이한 정렬, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,6 +1,3 @@
 function solution(numlist, n) {
-    return numlist.map((num, idx) => [Math.abs(n-num), num]).sort((a,b) => {
-        if (a[0] === b[0]) return b[1] - a[1];
-        return a[0] - b[0]
-    }).map(el => el[1])
+    return numlist.sort((a,b) => Math.abs(n-a) - Math.abs(n-b) || b-a)
 }
