ad5f025b6c87ecfc7f96b679cf03340db5e695ee
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-21 16:39:37 +0900

    [unrated] Title: 공원 산책, Time: 4.76 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
@@ -0,0 +1,27 @@
+function solution(park, routes) {
+    const [dy, dx] = [[-1, 0, 1, 0], [0, 1, 0, -1]];
+    const operation = {N:0, E:1, S:2, W:3};
+    
+    const [robot_y, robot_x] = park.reduce((acc, row, y_idx) => {
+        return acc.length === 0 ? [...row].reduce((acc, cur, x_idx) => cur === 'S' ? [y_idx, x_idx] : acc, []) : acc
+    }, [])
+    
+    console.log('robot: ', [robot_y, robot_x])
+    
+    return routes.reduce(([y, x], cur) => {
+        console.log('top: ', [y, x])
+        const [op, n] = cur.split(" ");
+        const [my, mx] = [y + dy[operation[op]] * n, x + dx[operation[op]] * n]
+        
+        if (my < 0 || mx < 0 || my >= park.length || mx >= park[0].length) return [y, x];
+        
+        for (let i=Math.min(y, my); i<=Math.max(y, my); i++) {
+            for (let j=Math.min(x, mx); j<=Math.max(x, mx); j++) {
+                if (park[i][j] === 'X') return [y, x];
+            }
+        }
+        
+        return [my, mx]
+        
+    }, [robot_y, robot_x])
+}
