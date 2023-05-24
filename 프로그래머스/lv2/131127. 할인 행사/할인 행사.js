fd4e8f04d6e11d91a1ba2fad4b18b4fb615122c1
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-24 13:51:59 +0900

    [level 2] Title: 할인 행사, Time: 0.21 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -1,14 +1,11 @@
 function solution(want, number, discount) {
+    const wantSchema = want.reduce((map, cur, idx) => map.set(cur, number[idx]), new Map);
+    
     return discount.reduce((acc, cur, idx, src) => {
-        const wantSchema = want.reduce((acc, cur, idx) => {
-            acc[cur] = number[idx]
-            return acc;
-        }, {});
         const successiveDiscount = src.slice(idx, idx + 10);
         
-        return acc + +Object.values(successiveDiscount.reduce((acc, cur) => {
-            if (acc[cur] && acc[cur] > 0) acc[cur]--;
-            return acc;
-        }, wantSchema)).every(n => n === 0)
+        return acc + +[...successiveDiscount.reduce((map, cur) => {
+            return map.set(cur, +map.get(cur) > 0 ? map.get(cur) - 1 : map.get(cur));
+        }, new Map(wantSchema)).values()].every(n => n === 0)
     }, 0)
 }
