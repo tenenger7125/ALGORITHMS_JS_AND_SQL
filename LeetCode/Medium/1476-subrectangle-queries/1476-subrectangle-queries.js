837fe2f36a24ea617b2cad65a10b134d63d44f41
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-26 11:14:13 +0900

    Time: 163 ms (5.15%), Space: 59.9 MB (5.15%) - LeetHub

---
+++ LeetCode/Medium/1476-subrectangle-queries/1476-subrectangle-queries.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} rectangle
+ */
+var SubrectangleQueries = function(rectangle) {
+    this.rectangle = rectangle;
+};
+
+/** 
+ * @param {number} row1 
+ * @param {number} col1 
+ * @param {number} row2 
+ * @param {number} col2 
+ * @param {number} newValue
+ * @return {void}
+ */
+SubrectangleQueries.prototype.updateSubrectangle = function(row1, col1, row2, col2, newValue) {
+    this.rectangle = this.rectangle.map((row, rowIdx) => row.map((col, colIdx) => {
+        if (row1 <= rowIdx && rowIdx <= row2 && col1 <= colIdx && colIdx <= col2) return newValue
+        return col;
+    }))
+};
+
+/** 
+ * @param {number} row 
+ * @param {number} col
+ * @return {number}
+ */
+SubrectangleQueries.prototype.getValue = function(row, col) {
+    return this.rectangle[row][col]
+};
+
+/** 
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * var obj = new SubrectangleQueries(rectangle)
+ * obj.updateSubrectangle(row1,col1,row2,col2,newValue)
+ * var param_2 = obj.getValue(row,col)
+ */
