46301afa5a8780d5bbbcdfa6151d7a9027c99de7
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-29 03:40:25 +0900

    [level 2] Title: N-Queen, Time: 1199.32 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.js
@@ -1,6 +1,8 @@
+// 여러 모범답안을 봤지만, 모범 답안과 다르게 구현했습니다.
 function solution(n) {
     let answer = 0;
     
+    // O(n)
     const dfs = (y, x, position) => {
         for (let i=0; i<position.length; i++) {
             const [py, px] = position[i];
@@ -10,11 +12,11 @@ function solution(n) {
         if (y === n-1) return answer++
         
         for (let i=0; i<n; i++) {
-            
             dfs(y+1, i, [...position, [y, x]]);
         }
     }
     
+    // O(1)
     for (let i=0; i<n; i++) {
         dfs(0, i, [])
     }
