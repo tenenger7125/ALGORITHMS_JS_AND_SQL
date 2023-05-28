b014479158d93d81b1275e4187252a92d53520d1
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-05-28 14:37:06 +0900

    [Silver V] Title: 나이순 정렬, Time: 440 ms, Memory: 57320 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -9,17 +9,8 @@ const arr = inputs
   .map((input) => input.split(" "))
   .map(([age, name]) => [+age, name]);
 
-const schema = arr.reduce((acc, [age, name], idx) => {
-  acc[name] = idx;
-  return acc;
-}, {});
-
 const result = arr
-  .sort((a, b) => {
-    if (a[0] === b[0]) return schema[a[0]] - schema[b[0]];
-
-    return a[0] - b[0];
-  })
+  .sort((a, b) => a[0] - b[0])
   .map((el) => el.join(" "))
   .join("\n");
 
