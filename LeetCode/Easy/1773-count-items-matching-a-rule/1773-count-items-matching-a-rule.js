5f2944aefefe55adbbabc03bc26edf353bc6cfb6
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-06-10 22:31:43 +0900

    Time: 77 ms (44.98%), Space: 46.3 MB (29.99%) - LeetHub

--- LeetCode/Easy/1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.js
+++ LeetCode/Easy/1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.js
@@ -5,7 +5,13 @@
  * @return {number}
  */
 var countMatches = function(items, ruleKey, ruleValue) {
-    return items.reduce((acc, [type, color, name]) => {
-        return acc + +((ruleKey === 'type' && ruleValue === type) || (ruleKey === 'color' && ruleValue === color) || (ruleKey === 'name' && ruleValue === name))
+    const schema = {
+        type: 0,
+        color: 1,
+        name: 2
+    }
+    
+    return items.reduce((acc, cur) => {
+        return acc + +(cur[schema[ruleKey]] === ruleValue)
     }, 0)
 };
