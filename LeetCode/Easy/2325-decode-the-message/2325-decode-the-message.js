5e3f882452cdec5723f93d55dcd22b5b4781b5f3
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-06-13 10:40:24 +0900

    Time: 61 ms (89.32%), Space: 45.1 MB (41.42%) - LeetHub

---
+++ LeetCode/Easy/2325-decode-the-message/2325-decode-the-message.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} key
+ * @param {string} message
+ * @return {string}
+ */
+var decodeMessage = function(key, message) {
+    const [code, schema] = [...key].reduce(([code, schema], char) => {
+        if (!schema[char]) schema[char] = String.fromCharCode(code++);
+        
+        return [code, schema]
+    }, [97, {" ": " "}])
+    console.log(schema)
+    return message.replace(/./g, match => schema[match])
+};
