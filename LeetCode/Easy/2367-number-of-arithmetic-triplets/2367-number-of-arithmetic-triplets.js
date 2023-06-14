2ae0365f5d684a048aaac1b26fcbd898cec0c068
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-06-14 12:58:10 +0900

    Time: 83 ms (28.92%), Space: 41.7 MB (88.35%) - LeetHub

---
+++ LeetCode/Easy/2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function(nums, diff) {
+    let answer = 0;
+    
+    for (let i=0; i<nums.length; i++) {
+        for (let j=i+1; j<nums.length; j++) {
+            for (let k=j+1; k<nums.length; k++) {
+                if (nums[j] - nums[i] === diff && nums[k] - nums[j] === diff) answer++;
+            }
+        }
+    }
+    return answer;
+};
