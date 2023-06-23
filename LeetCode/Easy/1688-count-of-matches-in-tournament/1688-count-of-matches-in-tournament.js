d1918982928ba2106fc1c2b6f842c8ecd873b458
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-06-23 21:16:51 +0900

    Time: 48 ms (94.84%), Space: 41.3 MB (97.74%) - LeetHub

--- LeetCode/Easy/1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ LeetCode/Easy/1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -6,10 +6,8 @@ var numberOfMatches = function(n) {
     let count = 0;
     while (n > 1) {
         const nextTeams = n % 2 === 0 ? n / 2 : Math.ceil(n / 2);
-        console.log(nextTeams)
         count += nextTeams + -(n % 2 !== 0);
         n = nextTeams;
-        console.log(count)
     }
     
     return count
