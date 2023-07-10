cd73beb7a6d5b41c4f51e8b9b6f5d147753438d6
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-07-11 00:29:58 +0900

    Time: 74 ms (66.03%), Space: 44.4 MB (65.46%) - LeetHub

--- LeetCode/Easy/2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.js
+++ LeetCode/Easy/2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var maximumNumberOfStringPairs = function(words) {
+    const map = new Map();
     let count = 0;
+    
     for (let i=0; i<words.length; i++) {
-        for (let j=i+1; j<words.length; j++) {
-            if (words[i] === words[j].split("").reverse().join("")) count++;
-        }
+        const word = words[i].split("").reverse().join("");
+        if (map.has(words[i])) count++;
+        map.set(word)
     }
     
     return count;
