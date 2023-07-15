e5ed930c78e85650e821742076b4860eda8bf9a3
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-07-15 23:41:28 +0900

    Time: 58 ms (86.32%), Space: 44.1 MB (39.82%) - LeetHub

--- LeetCode/Easy/1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
+++ LeetCode/Easy/1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
@@ -4,20 +4,17 @@
  */
 var removeOuterParentheses = function(s) {
     let check = 0;
-    let stack = [];
     let result = '';
     
-    
     for (const char of s) {
-        if (char === '(') check++;
-        else check--;
-        
-        stack.push(char);
-        
-        if (check === 0) {
-            result += stack.slice(1, -1).join("")
-            stack = [];
+        if (check === 0 && char === '(') {
+            check++;
+            continue;
         }
+        
+        check += char === '(' ? 1 : -1
+        
+        if (check !== 0) result += char
     }
     
     return result
