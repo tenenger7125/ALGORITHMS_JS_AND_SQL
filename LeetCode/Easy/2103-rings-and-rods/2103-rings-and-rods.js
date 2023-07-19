b39262f272f2ac1d0a95332ef770e9a7dcd015c6
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-07-19 11:11:47 +0900

    Time: 69 ms (8.65%), Space: 44.7 MB (5.29%) - LeetHub

---
+++ LeetCode/Easy/2103-rings-and-rods/2103-rings-and-rods.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} rings
+ * @return {number}
+ */
+var countPoints = function(rings) {
+    const obj = rings.match(/[RGB][0-9]/g).reduce((acc, cur) => {
+        const [color, index] = cur.split("");
+        
+        if (!acc[index]) acc[index] = {R: 0, G:0, B:0};
+        
+        acc[index][color]++;
+        return acc;
+    }, {})
+    console.log(obj)
+    return Object.values(obj).reduce((acc, cur) => {
+        const {R,G,B} = cur;
+        return acc + Math.min(R,G,B,1)
+    }, 0)
+};
