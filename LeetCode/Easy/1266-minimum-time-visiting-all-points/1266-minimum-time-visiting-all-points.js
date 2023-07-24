155146d089a535c8dba663f6bad8324853fe9e8f
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-07-25 00:51:21 +0900

    Time: 63 ms (64.34%), Space: 45.1 MB (5.43%) - LeetHub

--- LeetCode/Easy/1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.js
+++ LeetCode/Easy/1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.js
@@ -3,19 +3,14 @@
  * @return {number}
  */
 var minTimeToVisitAllPoints = function(points) {
-    let current = points[0];
     let count = 0
     
     for (let i=1; i<points.length; i++) {
-        const target = points[i];
-        const [cx, cy] = current;
-        const [tx, ty] = target;
-        
+        const [[cx, cy], [tx, ty]] = [points[i-1], points[i]]
         const absX = Math.abs(cx - tx);
         const absY = Math.abs(cy - ty);
         
         count += Math.min(absX, absY) + Math.abs(absX - absY);
-        current = target;
     }
     
     return count;
