aef1068be0e9a8a3c26dca5cd8e92ef155743a7e
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-07-31 15:08:36 +0900

    Update 오프라인／온라인 판매 데이터 통합하기.sql

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,11 +1,12 @@
-SELECT DATE_FORMAT(onsale.sales_date, '%Y-%m-%d'), onsale.product_id, user_id, onsale.sales_amount
-FROM online_sale AS onsale
-WHERE DATE_FORMAT(sales_date, '%Y-%m') ='2022-03'
+WITH TEMP_SALES AS (
+    SELECT DATE_FORMAT(SALES_DATE, "%Y-%m-%d") AS SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+    FROM ONLINE_SALE
+    UNION ALL
+    SELECT DATE_FORMAT(SALES_DATE, "%Y-%m-%d") AS SALES_DATE, PRODUCT_ID, NULL AS USER_ID, SALES_AMOUNT
+    FROM OFFLINE_SALE
+)
 
-UNION ALL
-
-SELECT DATE_FORMAT(offsale.sales_date, '%Y-%m-%d'), offsale.product_id, NULL AS user_id, offsale.sales_amount
-FROM offline_sale AS offsale
-WHERE DATE_FORMAT(sales_date, '%Y-%m') = '2022-03'
-
-ORDER BY 1 ASC , 2 ASC, 3 ASC;
+SELECT *
+FROM TEMP_SALES
+WHERE DATE_FORMAT(SALES_DATE, "%Y-%m") = "2022-03"
+ORDER BY 1, 2, 3
