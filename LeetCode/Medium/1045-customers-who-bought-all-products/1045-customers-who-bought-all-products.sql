2a0876770f89e91ebcd4c2d9f8c7ed23f1f10893
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2023-12-19 10:36:58 +0900

    Time: 1006 ms (63.50%), Space: 0B (100.00%) - LeetHub

--- LeetCode/Medium/1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ LeetCode/Medium/1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,5 +1,9 @@
 /* Write your PL/SQL query statement below */
 select customer_id
-from customer
+from (
+    select *
+    from customer
+    group by customer_id, product_key
+)
 group by customer_id
-having count(distinct product_key) = (select count(*) from product)
+having count(*) = (select count(*) from product)
