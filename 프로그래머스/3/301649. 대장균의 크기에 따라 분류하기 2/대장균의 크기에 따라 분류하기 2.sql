6b1db2d668eb017204fefb10cde0c91708c5f956
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2024-04-10 17:54:35 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,16 +1,15 @@
 select
     cte.id
     , case
-        when size_percentage / total * 100 <= 25 then 'CRITICAL'
-        when size_percentage / total * 100 <= 50 then 'HIGH'
-        when size_percentage / total * 100 <= 75 then 'MEDIUM'
+        when size_percent_rank <= 0.25 then 'CRITICAL'
+        when size_percent_rank <= 0.50 then 'HIGH'
+        when size_percent_rank <= 0.75 then 'MEDIUM'
         else 'LOW'
     end as colony_name
 from (
     select
         ed.id
-        , rank() over(order by ed.size_of_colony desc) as size_percentage
-        , (select count(*) from ecoli_data ed) as total
+        , percent_rank() over(order by ed.size_of_colony desc) as size_percent_rank
     from ecoli_data ed
 ) cte
 order by cte.id
