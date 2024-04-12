c3895084a96eaa99d1cd82a01e92c2eaf8150663
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2024-04-13 07:20:01 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,26 +1,18 @@
 select 
-    cte.emp_no
-    , cte.emp_name
-    , cte.grade
-    , cte.salary * case
-        when cte.grade = 'S' then 0.2
-        when cte.grade = 'A' then 0.15
-        when cte.grade = 'B' then 0.1
+    he.emp_no
+    , max(he.emp_name) as emp_name
+    , case
+        when avg(hg.score) >= 96 then 'S'
+        when avg(hg.score) >= 90 then 'A'
+        when avg(hg.score) >= 80 then 'B'
+        else 'C'
+    end as grade
+    , he.sal * case
+        when avg(hg.score) >= 96 then 0.2
+        when avg(hg.score) >= 90 then 0.15
+        when avg(hg.score) >= 80 then 0.1
         else 0
     end as bonus
-from (
-    select 
-        he.emp_no
-        , max(he.emp_name) as emp_name
-        , case
-            when avg(hg.score) >= 96 then 'S'
-            when avg(hg.score) >= 90 then 'A'
-            when avg(hg.score) >= 80 then 'B'
-            else 'C'
-        end as grade
-        , max(he.sal) as salary
-    from hr_grade hg
-        inner join hr_employees he
-            on hg.emp_no = he.emp_no
-    group by hg.emp_no, hg.year
-) cte
+from hr_employees he
+    inner join hr_grade hg on hg.emp_no = he.emp_no
+group by hg.emp_no, hg.year
